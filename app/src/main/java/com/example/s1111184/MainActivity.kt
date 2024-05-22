6ba06f5b062c365ccfa15217aed3652417347546
yuxian1121 <yuxian1121@github.com>
2024-05-22 15:56:37 +0800

    v1_陳語賢

--- app/src/main/java/com/example/s1111184/MainActivity.kt
+++ app/src/main/java/com/example/s1111184/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.example.s1111184
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.s1111184.ui.theme.S1111184Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +40,98 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             S1111184Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Main()
             }
         }
     }
 }
 
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Main() {
+    val navController = rememberNavController()
+    Scaffold(
+        topBar = {
+            TopAppBarWithMenu(navController)
+        }
+    ) {
+        NavHost(navController, startDestination = "JumpFirst") {
+            composable("JumpFirst") { FirstScreen(navController) }
+            composable("JumpSecond") { SecondScreen(navController) }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBarWithMenu(navController: androidx.navigation.NavController) {
+    var showMenu by remember { mutableStateOf(false) }
+    val context = LocalContext.current
+
+    TopAppBar(
+        title = {
+            // 使用 Image 替代 Text
+            Image(
+                painter = painterResource(id = R.drawable.maria),
+                contentDescription = "App you see",
+            )
+        },
+        actions = {
+            IconButton(
+                onClick = { showMenu = true }
+            ) {
+                Icon(Icons.Default.MoreVert, contentDescription = "More")
+            }
+            DropdownMenu(
+                expanded = showMenu, onDismissRequest = { showMenu = false }
+            ) {
+                DropdownMenuItem(
+                    text = { Text("簡介") },
+                    onClick = {
+                        navController.navigate("JumpFirst")
+                        showMenu = false
+                    }
+                )
+                DropdownMenuItem(
+                    text = { Text("主要機構") },
+                    onClick = {
+                        navController.navigate("JumpSecond")
+                        showMenu = false
+                    }
+                )
+            }
+        }
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    S1111184Theme {
-        Greeting("Android")
+fun FirstScreen(navController: NavController) {
+    Box(
+        contentAlignment = Alignment.TopStart, // 将文本對齊到屏幕左上角
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 60.dp)
+    ) {
+        Text(
+            text = "簡介",
+            color = Color.Blue
+        )
+    }
+}
+
+
+@Composable
+fun SecondScreen(navController: NavController) {
+    Box(
+        contentAlignment = Alignment.TopStart, // 将文本对齐到屏幕左上角
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 60.dp)
+    ) {
+        Text(
+            text = "主要機構",
+            color = Color.Red
+        )
     }
 }
